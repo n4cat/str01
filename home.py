b2af635db0c4168f910fbd17c4bc221cdcdccda1
n4cat <pondlowlife01@gmail.com>
2023-12-15 15:51:53 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,17 +1,4 @@
-import json
-from streamlit_lottie import st_lottie
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
-lottie3 = load_lottieurl(lot3)
-st_lottie(lottie3)
-
-    
+ 
 
 import streamlit as st
 
