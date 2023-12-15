61bcd10c4fa132b25f7421609c8d60846178355c
n4cat <pondlowlife01@gmail.com>
2023-12-15 14:36:21 +0700

    Update home.py

--- home.py
+++ home.py
@@ -12,3 +12,11 @@ with col1:
     st.image('./pic/profile-icon-design-free-vector.jpg')
 with col2:
     st.image('./pic/bahen_organic_milk_3_1024x1024.jpg')
+
+html_1 = """
+<div style="background-color:#7ED8FF;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
