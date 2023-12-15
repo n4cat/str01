362aab45ba567bfa436b6dd259eedab82d6eab7d
n4cat <pondlowlife01@gmail.com>
2023-12-15 15:57:09 +0700

    str01

--- home.py
+++ home.py
@@ -11,7 +11,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/profile-icon-design-free-vector.jpg')
+    st.image('./pic/mypro.jpg')
 with col2:
     st.image('./pic/bahen_organic_milk_3_1024x1024.jpg')
 
